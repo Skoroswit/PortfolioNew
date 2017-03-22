41917b40de0b6d901d17778c8053babd14752c7d
Michał <drobik@gmail.com>
2017-03-22 04:39:26 +0100

    init

---
+++ new portfolio/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.carousel').carousel();
+    $('.carousel').carousel('next', 3);
+    $(".button-collapse").sideNav();
+    Materialize.toast('Cześć!', 2500);
+});
+
